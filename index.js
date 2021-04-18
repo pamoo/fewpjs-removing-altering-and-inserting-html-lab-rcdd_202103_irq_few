bbf0795a05ab1c26e0f2380874eb53548b676694
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-18 20:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let main = document.getElementById('main');
 main.remove();
 
 let newHeader = document.createElement('h1');
-newHeader.set
+newHeader.setAttribute("victory", ID)
 newHeader.innerHTML = 'this is innerHtml text';
 document.body.appendChild(newHeader);
 
