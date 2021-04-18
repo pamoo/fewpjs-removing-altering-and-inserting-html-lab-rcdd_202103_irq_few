64dd48d8762785749425a064fd70fba398804144
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-18 20:23:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,5 @@ for (let i = 0; i < 3; i++) {
 element.appendChild(ul)
 let element = document.querySelector("p#greeting");
 element.innerHTML = 'Hello, DOM!'
+let header = document.getElementById("div#header");
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
