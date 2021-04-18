1c815c182b363eaa17ab8b73c735009f0f942bc4
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-18 20:34:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = 'Payam is the champion';
+document.body.appendChild(newHeader);
+
+
+
