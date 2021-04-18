5335a6e00a3260f691007e2d0b6c6b796bc9032c
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-18 20:28:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML = 'Abdulmajeed is the champion';
+newHeader.innerHTML = 'this is innerHtml text';
 document.body.appendChild(newHeader);
 
 
