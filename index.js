1050ad4cb2e50a603237ce69c482af8619f7ebf7
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-18 20:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ element.innerHTML = 'Hello, DOM!'
 let header = document.getElementById("div#header");
 header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
 content = someTextArea.value
-node.innerHTML = `Hi, ${content}!
+node.innerHTML = 'Hi, ${content}!'
+
+
+
